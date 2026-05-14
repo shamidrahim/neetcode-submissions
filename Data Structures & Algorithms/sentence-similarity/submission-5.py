9f66a11b51a0c6ed60cbd8d426e53b79058c45b4
shamidrahim <163294452+shamidrahim@users.noreply.github.com>
2026-05-14 20:08:50 +0500

    Add: sentence-similarity - submission-5

---
+++ Data Structures & Algorithms/sentence-similarity/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        if len(sentence1) != len(sentence2):
+            return False
+        mapping = defaultdict(set)
+        for word1, word2 in similarPairs:
+            mapping[word1].add(word2)
+            mapping[word2].add(word1)
+        for i in range(len(sentence1)):
+            if sentence1[i] == sentence2[i] or sentence2[i] in mapping[sentence1[i]]:
+                continue
+            return False
+        return True
+        
+        
+
+            
+        
