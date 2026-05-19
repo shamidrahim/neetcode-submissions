ac43be12c6d068516a506ccb839765e854280e9b
shamidrahim <163294452+shamidrahim@users.noreply.github.com>
2026-05-19 14:19:36 +0500

    Add: shortest-way-to-form-string - submission-11

---
+++ Data Structures & Algorithms/shortest-way-to-form-string/submission-11.py
@@ -0,0 +1,26 @@
+class Solution:
+    def shortestWay(self, source: str, target: str) -> int:
+        source_chars = set(source)
+        for char in target:
+            if char not in source_chars:
+                return -1
+        m = len(source)
+        
+        source_iterator = 0
+        count = 0
+        for char in target:
+            if source_iterator == 0:
+                count += 1
+            while source[source_iterator] != char:
+                source_iterator = (source_iterator + 1) % m
+
+                if source_iterator == 0:
+                    count += 1
+            source_iterator = (source_iterator + 1) % m
+        return count 
+
+
+
+
+        
+        
