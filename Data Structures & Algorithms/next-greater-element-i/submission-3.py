1c24384d8026a18c750518503e3e882cb18e85c6
shamidrahim <163294452+shamidrahim@users.noreply.github.com>
2026-05-25 23:54:31 +0500

    Add: next-greater-element-i - submission-3

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        num1Idx = {num : i for i, num in enumerate(nums1)}
+        res = [-1] * len(nums1)
+        for i in range(len(nums2)):
+            if nums2[i] not in num1Idx:
+                continue
+            for j in range(i + 1, len(nums2)):
+                if nums2[j] > nums2[i]:
+                    idx = num1Idx[nums2[i]]
+                    res[idx] = nums2[j]
+                    break
+        return res
+
+        
