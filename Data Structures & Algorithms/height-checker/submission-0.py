6f9b737cd4a351de82b89ae737956cf161cc1a6b
shamidrahim <163294452+shamidrahim@users.noreply.github.com>
2026-05-29 15:29:11 +0500

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+        return sum(h != e for h, e in zip(heights, expected))
+
+
+        
