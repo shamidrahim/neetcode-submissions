d4d8a5f6cb0a37c8151f468dde7190b45991f526
shamidrahim <163294452+shamidrahim@users.noreply.github.com>
2026-06-11 13:34:44 +0500

    Add: relative-sort-array - submission-2

---
+++ Data Structures & Algorithms/relative-sort-array/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        max_val = max(arr1)
+        count = [0] * (max_val + 1)
+        for num in arr1:
+            count[num] += 1
+
+        res = []
+        for num in arr2:
+            res += [num] * count[num]
+            count[num] = 0
+        for num in range(len(count)):
+            res += [num] * count[num]
+
+        return res
