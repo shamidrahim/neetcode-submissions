5ae3fbaddc1cf1e7e95a35418e1f6c47e64f202a
shamidrahim <163294452+shamidrahim@users.noreply.github.com>
2026-06-11 13:54:34 +0500

    Add: sort-the-people - submission-1

---
+++ Data Structures & Algorithms/sort-the-people/submission-1.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        arr = list(zip(heights, names))
+        arr.sort(reverse = True)
+        return [name for _, name in arr]
+        
