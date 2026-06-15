67964cc63e0817a8a009e44b255e4d7a1dd58182
shamidrahim <163294452+shamidrahim@users.noreply.github.com>
2026-06-15 10:36:23 +0500

    Add: best-time-to-buy-and-sell-stock-ii - submission-5

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        next_sell = next_buy = 0
+        cur_sell = cur_buy = 0
+        for i in range(n - 1, -1, -1):
+            cur_buy = max(next_buy, -prices[i] + next_sell)
+            cur_sell = max(next_sell, prices[i] + next_buy)
+            next_buy = cur_buy
+            next_sell = cur_sell
+        return cur_buy
+        
