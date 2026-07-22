66a947ba216b1cb752bc99b254df39dee52afe91
shamidrahim <163294452+shamidrahim@users.noreply.github.com>
2026-07-22 23:20:09 +0500

    Add: rearrange-array-elements-by-sign - submission-1

---
+++ Data Structures & Algorithms/rearrange-array-elements-by-sign/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            if ((i % 2 == 0 and nums[i] > 0) or
+                (i % 2 == 1 and nums[i] < 0)):
+                continue
+            
+            j = i + 1
+            while j < n and ((nums[j] > 0)) == (nums[i] > 0):
+                j += 1
+            
+            tmp = nums[j]
+            while j > i:
+                nums[j] = nums[j - 1]
+                j -= 1
+            nums[i] = tmp
+        return nums
+
+            
+
+
+        
